4eacfd66a6dd52c3c69331f4e083ed7d8b75b5c6
Alexandr Maspanov <alexandrmaspanov@gmail.com>
2025-06-29 00:13:24 +0300

    refactor: fix CustomSelect

--- src/components/customSelect/CustomSelect.jsx
+++ src/components/customSelect/CustomSelect.jsx
@@ -1,34 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './CustomSelect.module.css';
 
 const CustomSelect = ({
-    id,
-    value,
-    onChange,
-    options,
-    placeholder = 'Выберите значение',
-    disabled = false,
-    required = false,
+  id,
+  value,
+  onChange,
+  options,
+  placeholder = 'Выберите значение',
+  disabled = false,
+  required = false,
 }) => {
-    return (
-        <select
-            id={id}
-            className={styles.select}
-            value={value}
-            onChange={onChange}
-            disabled={disabled}
-            required={required}
-        >
-            <option value='' disabled hidden>
-                {placeholder}
-            </option>
-            {options.map(opt => (
-                <option key={opt.value} value={opt.value}>
-                    {opt.label}
-                </option>
-            ))}
-        </select>
-    );
-}
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      className={`${styles.selectWrapper} ${isOpen ? styles.open : ''}`}
+      onBlur={() => setIsOpen(false)}
+    >
+      <select
+        id={id}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+        required={required}
+        onClick={() => setIsOpen((prev) => !prev)}
+        className={styles.select}
+      >
+        <option value="" disabled hidden>
+          {placeholder}
+        </option>
+        {options.map((opt) => (
+          <option key={opt.value} value={opt.value}>
+            {opt.label}
+          </option>
+        ))}
+      </select>
+      <span className={styles.arrow} />
+    </div>
+  );
+};
 
 export default CustomSelect;
