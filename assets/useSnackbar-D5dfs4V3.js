462fe23be2c1d48a3db512ffdbb4c84dba0acb95
Alexandr Maspanov <alexandrmaspanov@gmail.com>
2025-07-01 01:13:07 +0300

    Updates

--- assets/useSnackbar-D5dfs4V3.js
+++
@@ -1,1 +0,0 @@
-import{x as r}from"./index-8NbFKH9F.js";const o=()=>{const{showSnackbar:a}=r();return{showSnackbar:a}};export{o as u};
---
+++ assets/useSnackbar-D8PNpjkH.js
@@ -0,0 +1,1 @@
+import{x as r}from"./index-DD8JO9LC.js";const o=()=>{const{showSnackbar:a}=r();return{showSnackbar:a}};export{o as u};
