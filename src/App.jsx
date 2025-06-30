289a2eb0f56e1b9abc015c8297a7c8df20f23a25
Alexandr Maspanov <alexandrmaspanov@gmail.com>
2025-07-01 01:12:52 +0300

    fix: correct basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from './router/routes';
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+  basename: '/todo-trainer/',
+  future: {
+    v7_startTransition: true,
+  },
+});
 
 function App() {
   return <RouterProvider router={router} />;
